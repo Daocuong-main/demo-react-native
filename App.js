9ad5b8f1939321efb2f1db367cecbab055b3c388
Dao Cuong <cuongsieunhancsn@gmail.com>
2022-10-16 16:51:04 +0700

    update 2

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ActivityIndicator_demo from './src/components/ActivityIndicator_demo';
+import Button_demo from './src/components/Button_demo';
+import FlatList_demo from './src/components/FlatList_demo';
+import DisplayAnImage from './src/components/DisplayAnImage';
+import ImageBackground_demo from './src/components/ImageBackground_demo';
+import KeyboardAvoidingComponent from './src/components/KeyboardAvoidingComponent';
+import Modal_demo from './src/components/Modal_demo';
+import Pressable_demo from './src/components/Pressable_demo';
+import RefreshControl_demo from './src/components/RefreshControl_demo';
+import ScrollView_demo from './src/components/ScrollView_demo';
+import SectionList_demo from './src/components/SectionList_demo';
+import StatusBar_demo from './src/components/StatusBar_demo';
+import Switch_demo from './src/components/Switch_demo';
+import TextInANest from './src/components/TextInANest';
+import UselessTextInput from './src/components/UselessTextInput';
+import TouchableHighlight_demo from './src/components/TouchableHighlight_demo';
+import TouchableOpacity_demo from './src/components/TouchableOpacity_demo';
+import TouchableWithoutFeedbackExample from './src/components/TouchableWithoutFeedbackExample';
+import View_demo from './src/components/View_demo';
+import VirtualizedList_demo from './src/components/VirtualizedList_demo';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // <ActivityIndicator_demo />
+    // <Button_demo/>
+    // <FlatList_demo/>
+    // <DisplayAnImage/>
+    // <ImageBackground_demo/>
+    // <KeyboardAvoidingComponent/>
+    // <Modal_demo/>
+    // <Pressable_demo/>
+    // <RefreshControl_demo/>
+    // <ScrollView_demo/>
+    // <SectionList_demo/>
+    // <StatusBar_demo/>
+    // <Switch_demo/>
+    // <TextInANest/>
+    // <UselessTextInput/>
+    // <TouchableHighlight_demo/>
+    // <TouchableOpacity_demo/>
+    // <TouchableWithoutFeedbackExample/>
+    // <View_demo/>
+    <VirtualizedList_demo/>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
