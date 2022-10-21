f9f54637f8103b4b02e3dd5be296c193cebb3dcf
Dao Cuong <cuongsieunhancsn@gmail.com>
2022-10-21 07:31:44 +0700

    update 4

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
+import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import CreateAccount from "./Screen/CreateAccount";
 import LoginTemplate from "./Screen/LoginTemplate";
 import WelcomeBack from "./Screen/WelcomeBack";
 const App = () => {
- <CreateAccount/>
+  return (
+    <CreateAccount />
+  )
 };
-
-
-export default App;
